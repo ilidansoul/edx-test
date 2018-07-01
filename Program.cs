5d01da80ac5c6f83c7bbdf15a6b3c26354f9a016
Razor <ilidansoulkeeper@gmail.com>
2018-07-01 20:52:15 +0700

    merapikan comment, spasi dan baris

--- Program.cs
+++ Program.cs
@@ -10,30 +10,37 @@ namespace dev275x.studentlist
         {
            
             /* Check arguments */
-            if (args[0] == "a") {
+            if (args[0] == "a") 
+			{
                 Console.WriteLine("Loading data ...");
                 var s = new FileStream("students.txt",FileMode.Open);
                 var r = new StreamReader(s);
-                var D = r.ReadToEnd(); var i = D.Split(',');
-                foreach(var j in i) {Console.WriteLine(j);}
+                var D = r.ReadToEnd(); 
+				var i = D.Split(',');
+                foreach(var j in i) 
+				{
+					Console.WriteLine(j);
+				}
                 Console.WriteLine("Data loaded");
             }
             else if (args[0]== "r")
             {
                 Console.WriteLine("Loading data ...");
+				
                 // We are loading data
                 var s = new FileStream("students.txt",FileMode.Open);
                 var r = new StreamReader(s);
                 var d = r.ReadToEnd();
                 var i = d.Split(',');
                 var x = new Random();
-                    var y = x.Next(0,i.Length);
-                        Console.WriteLine(i[y]);
+                var y = x.Next(0,i.Length);
+                Console.WriteLine(i[y]);
                 Console.WriteLine("Data loaded");
             }
 
             else if (args[0].Contains("+"))
             {
+				
                 // read
                 Console.WriteLine("Loading data ...");
                 var s = new FileStream("students.txt",FileMode.Open);
@@ -42,6 +49,7 @@ namespace dev275x.studentlist
                 var g = new StreamReader(s);
                 var d = g.ReadToEnd();
                 s.Seek(0,SeekOrigin.Begin);
+				
                 // Write
                 // But we're in trouble if there are ever duplicates entered
                 r.WriteLine(d.Replace('\n',' ') + "," + t);
@@ -78,7 +86,8 @@ namespace dev275x.studentlist
                 {
                     if (c > ' ' && c < 0177)
                     {
-                        if (!in_word) {
+                        if (!in_word) 
+						{
                             count = count + 1;
                             in_word = true;
                         }
@@ -87,10 +96,8 @@ namespace dev275x.studentlist
                         in_word = false;
                     }
                 }
-
                 Console.WriteLine(String.Format("{0} words found", count));
             }
-            
         }
     }
 }
